6cb98b0098dfc4d96b65756cefaed50fb5ba6764
siweiZhu <thinkzhu110929@gmail.com>
2021-01-20 22:45:38 +0800

    完成第一章
    使用Stacks来管理视图

---
+++ ScrumDinger/MettingView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  ScrumDinger
+//
+//  Created by siweiZhu on 2021/1/20.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Second Elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                
+                Spacer()
+                
+                VStack(alignment: .trailing) {
+                    Text("Second Remaining")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.tophalf.fill")
+                }
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel(Text("Time remaining"))
+            .accessibilityValue(Text("10 minutes"))
+            
+            Circle()
+                .strokeBorder(lineWidth: 24, antialiased: true)
+            
+            HStack {
+                Text("Speaker 1 of 3")
+                
+                Spacer()
+                
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+                .accessibilityLabel("Next speaker")
+            }
+        }
+        .padding()
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}

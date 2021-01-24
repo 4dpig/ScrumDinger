8a6914e003642c2e5ffcb35ea32b147203cdcd23
siweiZhu <thinkzhu110929@gmail.com>
2021-01-24 21:19:34 +0800

    添加CardView

---
+++ ScrumDinger/CardView.swift
@@ -0,0 +1,57 @@
+//
+//  CardView.swift
+//  ScrumDinger
+//
+//  Created by siweiZhu on 2021/1/21.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(scrum.title)
+                .font(.headline)
+            
+            Spacer()
+            
+            HStack {
+                Label(
+                    String(scrum.attendees.count),
+                    systemImage: "person.3"
+                )
+                .accessibilityElement(children: .ignore)
+                .accessibilityLabel(Text("Attendees"))
+                .accessibilityValue(
+                    Text(String(scrum.attendees.count))
+                )
+                
+                Spacer()
+                
+                Label(
+                    String(scrum.lengthInMinutes),
+                    systemImage: "clock"
+                )
+                .padding(.trailing, 20)
+                .accessibilityElement(children: .ignore)
+                .accessibilityLabel(Text("Meeting length"))
+                .accessibilityValue(
+                    Text("\(scrum.lengthInMinutes) minutes")
+                )
+            }
+            .font(.caption)
+        }
+        .padding()
+        .foregroundColor(scrum.color.accessibleFontColor)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var scrum = DailyScrum.data[0]
+    static var previews: some View {
+        CardView(scrum: scrum)
+            .background(scrum.color)
+            .previewLayout(.fixed(width: 400, height: 80))
+    }
+}
